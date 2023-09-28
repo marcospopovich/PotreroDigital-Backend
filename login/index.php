c36acda366cb5ac02405b3db914f5e7cfc82351a
Marcos Ariel Popovich <59690249+marcospopovich@users.noreply.github.com>
2023-09-28 00:35:09 -0300

    Commit con cambios en interface y login Hardcodeado

--- login/index.php
+++ login/index.php
@@ -1,13 +1,20 @@
 <?php
 $alert = "";
-$mensaje = "";
-if(!empty($_POST)){
+$flag = "";
+$email = 'admin@admin';
+$passwd = 1234;
+if (!empty($_POST)) {
 
-    if (!empty($_POST["registro"])){
-		$mensaje = "registro";
+	if (!empty($_POST["registro"])) {
+		$flag = "registro";
 	}
-	if(!empty($_POST["login"])){
-		$mensaje = "login";
+	if (!empty($_POST["login"])) {
+		if ($_POST['emailLogin'] == $email & $_POST['pswdLogin'] == $passwd) {
+			$flag = "login";
+		} else {
+			$flag = "error";
+		}
+
 	}
 
 
@@ -16,37 +23,73 @@ if(!empty($_POST)){
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Slide Navbar</title>
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
-<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
 </head>
+
 <body>
-	<div class="main">  	
+	<div class="main">
 		<input type="checkbox" id="chk" aria-hidden="true">
 
-			<div class="signup">
-				<form action="" method="post" autocomplete="off">
-					<label for="chk" aria-hidden="true">Registro</label>
-					<input type="hidden" name="registro" value="registro" />
-					<input type="text" name="txt" placeholder="Usuario" required="">
-					<input type="email" name="email" placeholder="Email" required="">
-					<input type="password" name="pswd" placeholder="Password" required="">
-					<button>Ingresar</button>
-				</form>
-			</div>
+		<div class="signup">
+			<?php
 
-			<div class="login">
-				<form action="" method="post" autocomplete="off">
+			switch ($flag) {
+				case '':
+					print
+						'<form action="" method="post" autocomplete="off">
+								<label for="chk" aria-hidden="true">Registro</label>
+								<input type="hidden" name="registro" value="registro" />
+								<input type="text" name="txt" placeholder="Usuario" required="">
+								<input type="email" name="email" placeholder="Email" required="">
+								<input type="password" name="pswd" placeholder="Password" required="">
+								<button>Ingresar</button>
+							</form>';
+					break;
+				case 'registro':
+					echo '<div id="registro"> <h2>Registro Exitoso</h2></div>
+					
+					';
+					break;
+				case 'login':
+					echo '<div id="registro"> <h2>Bienvenido al sistema</h2></div>';
+					break;
+				case 'error':
+					echo '<div id="registro"> <h2>Datos Invalidos</h2></div>';
+					break;
+			}
+
+
+			?>
+		</div>
+
+		<div class="login">
+			<?php
+			if ($flag == "") {
+				print('<form action="" method="post" autocomplete="off">
 				<input type="hidden" name="login" value="login" />
-					<label for="chk" aria-hidden="true">Login</label>
-					<input type="email" name="emailLogin" placeholder="Email" required="">
-					<input type="password" name="pswdLogin" placeholder="Password" required="">
-					<button>Login</button>
-					<h6>Olvidaste la contraseña?</h6>	
-				</form>
-			</div>
-	</div>
-</body>
-</html>
+				<label for="chk" aria-hidden="true">Login</label>
+				<input type="email" name="emailLogin" placeholder="Email" required="">
+				<input type="password" name="pswdLogin" placeholder="Password" required="">
+				<button>Login</button>
+				<div id="contrasenia">
+					<a href="https://es.wikipedia.org/wiki/Adam_Elsheimer">
+						<h6>Olvidaste la contraseña?</h6>
+					</a>
 
+				</div>
+
+			</form>');
+			}
+
+			?>
+		</div>
+	</div>
+
+
+</body>
+
+</html>
